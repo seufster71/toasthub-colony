3f02ecc0144db70ffb49132e5329089f8c88edfb
seufster71 <seufster@gmail.com>
2023-05-24 10:00:11 -0400

    init setup

---
+++ toasthub-colony/src/main/java/org/toasthub/ToasthubColonyApplication.java
@@ -0,0 +1,13 @@
+package org.toasthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToasthubColonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToasthubColonyApplication.class, args);
+	}
+
+}
